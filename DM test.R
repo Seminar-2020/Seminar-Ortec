03a7004cfb9bfbe03f30ace9049588c1e7222a59
Rosatk96 <59684288+Rosatk96@users.noreply.github.com>
2020-03-05 21:00:31 +0100

    Update DM test.R

--- DM test.R
+++ DM test.R
@@ -1,41 +1,78 @@
+# Bunk & Train set
+# Random forest versus GB, NN and OLS
+NN.f <- NN.AP.train[,2]
+GB.f <- GBM.AP.train[,2]
+RF.f <- RF.AP.train[,2]
+OLS.f <- OLS.AP.train[,2]
+y <- OLS.AP.train[,1]
+
+RFvsGB.train.bunk <- DM.test(RF.f, GB.f, y, loss.type = "AE")
+RFvsNN.train.bunk <- DM.test(RF.f, NN.f, y, loss.type = "AE")
+RFvsOLS.train.bunk <- DM.test(OLS.f, RF.f, y, loss.type = "AE")
+
+#GB versus NN en OLS
+GBvsNN.train.bunk <- DM.test(NN.f, GB.f, y, loss.type = "AE")
+GBvsOLS.train.bunk <- DM.test(OLS.f, GB.f, y, loss.type = "AE")
+
+#NN versus OLS
+NNvsOLS.train.bunk <- DM.test(NN.f, OLS.f, y, loss.type = "AE")
+
+#############################################################
+# Non-Bunk & Train set
+# Random forest versus GB, NN and OLS
+NN.f.NAP <- NN.NAP.train[,2]
+GB.f.NAP <- GBM.NAP.train[,2]
+RF.f.NAP <- RF.NAP.train[,2]
+OLS.f.NAP <- OLS.NAP.train[,2]
+y.NAP <- OLS.NAP.train[,1]
+
+RFvsGB.train.nbunk <- DM.test(RF.f.NAP, GB.f.NAP, y.NAP, loss.type = "AE")
+RFvsNN.train.nbunk <- DM.test(RF.f.NAP, NN.f.NAP, y.NAP, loss.type = "AE")
+RFvsOLS.train.nbunk <- DM.test(OLS.f.NAP, RF.f.NAP, y.NAP, loss.type = "AE")
+
+#GB versus NN en OLS
+GBvsNN.train.nbunk <- DM.test(NN.f.NAP, GB.f.NAP, y.NAP, loss.type = "AE")
+GBvsOLS.train.nbunk <- DM.test(OLS.f.NAP, GB.f.NAP, y.NAP, loss.type = "AE")
+
+#NN versus OLS
+NNvsOLS.train.nbunk <- DM.test(NN.f.NAP, OLS.f.NAP, y.NAP, loss.type = "AE")
+
+##########################################################
 # Bunk & Test set
 # Random forest versus GB, NN and OLS
-#NN.f <- exp(NN[,16])
-GB.f <- exp(GBM[,16])
-RF.f <- exp(RF[,16])
-OLS.f <- exp(OLS[,16])
-y <- exp(RF[,1])
+NN.f.test <- NN.AP.test[,2]
+GB.f.test <- GBM.AP.test[,2]
+RF.f.test <- RF.AP.test[,2]
+OLS.f.test <- OLS.AP.test[,2]
+y.test <- OLS.AP.test[,1]
 
-RFvsGB.test.bunk <- DM.test(RF.f, GB.f, y, loss.type = "AE")
-#RFvsNN.test.bunk <-
-RFvsOLS.test.bunk <- DM.test(OLS.f, RF.f, y, loss.type = "AE")
+RFvsGB.test.bunk <- DM.test(RF.f.test, GB.f.test, y.test, loss.type = "AE")
+RFvsNN.test.bunk <- DM.test(RF.f.test, NN.f.test, y.test, loss.type = "AE")
+RFvsOLS.test.bunk <- DM.test(OLS.f.test, RF.f.test, y.test, loss.type = "AE")
 
-#Bunk & Test set
 #GB versus NN en OLS
-#GBvsNN.test.bunk
-GBvsOLS.test.bunk <- DM.test(OLS.f, GB.f, y, loss.type = "AE")
+GBvsNN.test.bunk <- DM.test(NN.f.test, GB.f.test, y.test, loss.type = "AE")
+GBvsOLS.test.bunk <- DM.test(OLS.f.test, GB.f.test, y.test, loss.type = "AE")
 
-#Bunk & Test set
 #NN versus OLS
-#NNvsOLS.test.bunk <- DM.test(NN.f, OLS.f, y, loss.type = "AE")
+NNvsOLS.test.bunk <- DM.test(NN.f.test, OLS.f.test, y.test, loss.type = "AE")
 
-#Non-bunk & Test
-GB.f.NA <- exp(GBM.NA[,16])
-RF.f.NA <- exp(RF.NA[,16])
-OLS.f.NA <- exp(OLS.NA[,16])
-y.NA <- exp(RF.NA[,1])
+#########################################################
+# Non-Bunk & Test set
+# Random forest versus GB, NN and OLS
+NN.f.test.NAP <- NN.NAP.test[,2]
+GB.f.test.NAP <- GBM.NAP.test[,2]
+RF.f.test.NAP <- RF.NAP.test[,2]
+OLS.f.test.NAP <- OLS.NAP.test[,2]
+y.test.NAP <- OLS.NAP.test[,1]
 
-RFvsGB.test.nonbunk <- DM.test(RF.f.NA, GB.f.NA, y.NA, loss.type = "AE")
-#RFvsNN.test.bunk <-
-RFvsOLS.test.nonbunk <- DM.test(OLS.f.NA, RF.f.NA, y.NA, loss.type = "AE")
+RFvsGB.test.nbunk <- DM.test(RF.f.test.NAP, GB.f.test.NAP, y.test.NAP, loss.type = "AE")
+RFvsNN.test.nbunk <- DM.test(RF.f.test.NAP, NN.f.test.NAP, y.test.NAP, loss.type = "AE")
+RFvsOLS.test.nbunk <- DM.test(OLS.f.test.NAP, RF.f.test.NAP, y.test.NAP, loss.type = "AE")
 
-#Bunk & Test set
 #GB versus NN en OLS
-#GBvsNN.test.bunk
-GBvsOLS.test.nonbunk <- DM.test(OLS.f.NA, GB.f.NA, y.NA, loss.type = "AE")
+GBvsNN.test.nbunk <- DM.test(NN.f.test.NAP, GB.f.test.NAP, y.test.NAP, loss.type = "AE")
+GBvsOLS.test.nbunk <- DM.test(OLS.f.test.NAP, GB.f.test.NAP, y.test.NAP, loss.type = "AE")
 
-#Bunk & Test set
 #NN versus OLS
-#NNvsOLS.test.bunk <- DM.test(NN.f, OLS.f, y, loss.type = "AE")
-
-
+NNvsOLS.test.nbunk <- DM.test(NN.f.test.NAP, OLS.f.test.NAP, y.test.NAP, loss.type = "AE")
